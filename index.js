c5cd6c7ce7c5c04446e0ea7a1ab2e0aeced5b63b
Michael Czechowski <m.czechowski@pons.de>
2025-04-11 10:09:00 +0200

    refactor HTML and JavaScript for improved readability and consistency

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import {WebSocketServer} from "ws";
-
-const port = process.env.PORT || 8081;
-
-const wss = new WebSocketServer({port});
+const wss = new WebSocketServer({
+	port: 8080
+});
 
 wss.on("connection", (ws) => {
 	ws.id = Date.now();
